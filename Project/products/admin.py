6ca26c5169574776d4d0c3034c88d0cf4c838087
QRage <sergiobamper@gmail.com>
2025-06-14 22:44:52 +0300

    Refactor product model: added dynamic attributes and admin integration for attribute management and images

--- Project/products/admin.py
+++ Project/products/admin.py
@@ -1,9 +1,38 @@
 from django.contrib import admin
+from django.utils.html import format_html
 
-from .models import Category
+from .models import Category, Product, ProductAttributeValue, ProductAttribute, ProductImage
+
+
+class ProductAttributeValueInline(admin.TabularInline):
+    model = ProductAttributeValue
+    extra = 1
+    autocomplete_fields = ['attribute']
+
+
+@admin.register(ProductAttribute)
+class ProductAttributeAdmin(admin.ModelAdmin):
+    search_fields = ['name']
+
+
+class ProductImageInline(admin.TabularInline):
+    model = ProductImage
+    extra = 1
+    readonly_fields = ['preview']
+
+    def preview(self, obj):
+        if obj.image:
+            return format_html('<img src="{}" width="100" height="100" style="object-fit: cover;" />', obj.image.url)
+        return ""
+    preview.short_description = "Preview"
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    inlines = [ProductAttributeValueInline, ProductImageInline]
 
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ('name', 'slug')
+    list_display = ['name', 'slug']
     prepopulated_fields = {'slug': ('name',)}
--- Project/products/models.py
+++ Project/products/models.py
@@ -1,35 +1,10 @@
+# products/models.py
+
 from django.utils.translation import gettext_lazy as _
 from django.utils.text import slugify
 from django.db.models import Max
-
 from django.db import models
 
-
-class Product(models.Model):
-    title = models.CharField(max_length=255)
-    description = models.TextField()
-    article = models.CharField(max_length=10, unique=True, blank=True)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    in_stock = models.BooleanField(default=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    category = models.ForeignKey(
-    'Category', on_delete=models.SET_NULL, null=True, blank=True,
-    related_name='products', verbose_name=_("Category")
-    )
-
-    def save(self, *args, **kwargs):
-        if not self.article:
-            last_id = Product.objects.aggregate(Max('id'))['id__max'] or 0
-            next_id = last_id + 1
-            self.article = f"{next_id:05d}"
-        super().save(*args, **kwargs)
-
-
-class ProductImage(models.Model):
-    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='images')
-    image = models.ImageField(upload_to='products/')
-    uploaded_at = models.DateTimeField(auto_now_add=True)
-    
 
 class Category(models.Model):
     name = models.CharField(max_length=100, verbose_name=_("Name"))
@@ -46,3 +21,60 @@ class Category(models.Model):
         if not self.slug:
             self.slug = slugify(self.name)
         super().save(*args, **kwargs)
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    article = models.CharField(max_length=10, unique=True, blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    in_stock = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    category = models.ForeignKey(
+        Category, on_delete=models.SET_NULL, null=True, blank=True,
+        related_name='products', verbose_name=_("Category")
+    )
+
+    def __str__(self):
+        return self.title
+
+    def save(self, *args, **kwargs):
+        if not self.article:
+            last_id = Product.objects.aggregate(Max('id'))['id__max'] or 0
+            next_id = last_id + 1
+            self.article = f"{next_id:05d}"
+        super().save(*args, **kwargs)
+
+    def get_attributes(self):
+        return self.attributes.select_related('attribute').all()
+
+
+class ProductImage(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='images')
+    image = models.ImageField(upload_to='products/')
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+
+
+class ProductAttribute(models.Model):
+    name = models.CharField(max_length=100, unique=True, verbose_name=_("Attribute Name"))
+
+    class Meta:
+        verbose_name = _("Product Attribute")
+        verbose_name_plural = _("Product Attributes")
+
+    def __str__(self):
+        return self.name
+
+
+class ProductAttributeValue(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='attributes')
+    attribute = models.ForeignKey(ProductAttribute, on_delete=models.CASCADE, related_name='values')
+    value = models.CharField(max_length=255, verbose_name=_("Attribute Value"))
+
+    class Meta:
+        unique_together = ('product', 'attribute')
+        verbose_name = _("Product Attribute Value")
+        verbose_name_plural = _("Product Attribute Values")
+
+    def __str__(self):
+        return f"{self.attribute.name}: {self.value}"
